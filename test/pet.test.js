d118f69c957597a771094cbdbefd2e9d3a16fd84
Andreea Ros <ros.andreeacatalina@gmail.com>
2020-05-13 12:18:24 +0100

    Added age property and growUp method.

--- test/pet.test.js
+++ test/pet.test.js
@@ -1,10 +1,23 @@
 const Pet = require('../src/pet');
 
 describe("Pet", () => {
-  it("returns a new Pet object", () => {
-  expect(new Pet('Alex')).toBeInstanceOf(Object)})})
-
+  test("returns a new Pet object", () => {
+  expect(new Pet('Alex')).toBeInstanceOf(Object)});})
 
 describe("Pet", () => {
   it("returns a new Pet object with the property 'name'", () =>{
   expect(new Pet('Alex').name).toEqual('Alex')})})
+
+describe("Pet.age", () => {
+  it("returns a Pet object's age property, initially set to 0", () => {
+    expect(new Pet('Andy').age).toEqual(0);
+  })
+});
+
+describe("Pet.growUp()", () => {
+  const testPet = new Pet('Andy')
+  testPet.growUp()
+  it("increases a Pet object's age in 1 increments", () => {
+    expect(testPet.age).toEqual(1);
+  })
+})
